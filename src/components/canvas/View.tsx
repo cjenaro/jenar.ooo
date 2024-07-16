6ef150c9dd50146bf75d8a96005a29ff9cd190f9
Jenaro Calviño <jen.calvineo@gmail.com>
2024-07-16 12:33:48 -0300

    moved animation

--- src/components/canvas/View.tsx
+++ src/components/canvas/View.tsx
@@ -4,8 +4,6 @@ import { forwardRef, Suspense, useImperativeHandle, useRef } from 'react'
 import { OrbitControls, PerspectiveCamera, View as ViewImpl } from '@react-three/drei'
 import { Three } from '@/helpers/components/Three'
 import { useControls } from 'leva'
-import { useGSAP } from '@gsap/react'
-import gsap from 'gsap'
 
 export type CommonProps = {
   color?: string
@@ -22,32 +20,13 @@ export const Common = ({ color }: CommonProps) => {
     fogColor: '#ff618a',
   })
 
-  const cameraRef = useRef(null)
-  const cameraPosition = useRef({ x: 9, y: 0, z: 8 })
-
-  useGSAP(() => {
-    if (cameraRef.current) {
-      gsap.to(cameraRef.current.position, {
-        x: 0,
-        y: 0.9,
-        z: 3.5,
-        duration: 1,
-      })
-    }
-  })
-
   return (
     <Suspense fallback={null}>
       {color && <color attach='background' args={[color]} />}
       <ambientLight intensity={1} />
       <fog attach='fog' color={fogColor} near={fog.near} far={fog.far} />
       <directionalLight castShadow color='#ff618a' position={lightPosition} intensity={intensity} />
-      <PerspectiveCamera
-        makeDefault
-        ref={cameraRef}
-        fov={40}
-        position={[cameraPosition.current.x, cameraPosition.current.y, cameraPosition.current.z]}
-      />
+      <PerspectiveCamera makeDefault fov={50} position={[9, 0, 8]} />
     </Suspense>
   )
 }
