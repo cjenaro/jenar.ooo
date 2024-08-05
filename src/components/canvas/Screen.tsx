3ae24dca15ef30f0587f22916ecde1fe6076e391
Jenaro Calviño <jen.calvineo@gmail.com>
2024-08-05 16:42:10 -0300

    feat: use menu screen

--- src/components/canvas/Screen.tsx
+++ src/components/canvas/Screen.tsx
@@ -1,6 +1,7 @@
 import { toVec3, toEuler } from '@/helpers/three-helpers'
 import { Html } from '@react-three/drei'
 import { folder, useControls } from 'leva'
+import Link from 'next/link'
 import { useEffect, useRef, useState } from 'react'
 
 function focusElement(element?: HTMLElement) {
@@ -71,35 +72,63 @@ export default function Screen({ geometry, material }) {
         distanceFactor={1}
       >
         {!loadGame ? (
-          <div className='game-menu'>
-            <h1>Jenaro Calviño</h1>
-            <p>This is my resume, use the keyboard arrows to navigate the menu</p>
-            <nav>
-              <button className='focused' ref={ctaRef} onClick={handleLoadGame}>
-                Play Game!
-              </button>
-              <a ref={linkRef} href='/cv'>
-                Take me to a regular CV
-              </a>
-            </nav>
+          <div className='w-full h-full relative rounded-[52px] overflow-hidden'>
+            <div className='w-full h-full absolute z-0'>
+              <div className='bg img'></div>
+              <div className='clouds img'></div>
+              <div className='clouds-front img'></div>
+              <div className='beach img'></div>
+              <div className='char img'></div>
+            </div>
+            <div className='game-menu relative z-10 rounded bg-pink-50/50'>
+              <h1>Jenaro Calviño</h1>
+              <p>This is my resume, use the keyboard arrows to navigate the menu</p>
+              <nav>
+                <button className='focused' ref={ctaRef} onClick={handleLoadGame}>
+                  Play Game!
+                </button>
+                <Link ref={linkRef} href='/cv'>
+                  Take me to a regular CV
+                </Link>
+              </nav>
+            </div>
           </div>
         ) : (
-          <iframe
-            src='/game'
-            allowTransparency
-            allowFullScreen
-            frameBorder={0}
-            scrolling='no'
-            allow='autoplay; fullscreen *; geolocation; microphone; camera; midi; monetization; xr-spatial-tracking; gamepad; gyroscope; accelerometer; xr; cross-origin-isolated; web-share'
-            style={{
-              width: '760px',
-              height: '450px',
-              border: 'none',
-              transform: 'scale(1)',
-              transformOrigin: 'center center',
-              borderRadius: '52px',
-            }}
-          />
+          <>
+            <Link href='/game' className='absolute bottom-6 right-6 bg-pink-50/50 rounded w-8 h-8 z-10'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='size-6'
+              >
+                <title>Fullscreen</title>
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
+                />
+              </svg>
+            </Link>
+            <iframe
+              src='/game'
+              allowTransparency
+              allowFullScreen
+              frameBorder={0}
+              scrolling='no'
+              allow='autoplay; fullscreen *; geolocation; microphone; camera; midi; monetization; xr-spatial-tracking; gamepad; gyroscope; accelerometer; xr; cross-origin-isolated; web-share'
+              style={{
+                width: '760px',
+                height: '450px',
+                border: 'none',
+                transform: 'scale(1)',
+                transformOrigin: 'center center',
+                borderRadius: '52px',
+              }}
+            />
+          </>
         )}
       </Html>
     </mesh>
